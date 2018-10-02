109d6dba17bc62e4e1e87dd71dd4c0770b72b1bc
hendratay <hendratay96@gmail.com>
2018-10-02 13:27:27 +0700

    add socket io

---
+++ app/src/main/java/com/example/user/sync_media_player/Socket.kt
@@ -0,0 +1,27 @@
+package com.example.user.sync_media_player
+
+import android.util.Log
+import io.socket.client.IO
+import io.socket.client.Socket
+
+object Socket {
+
+    private val socket = IO.socket("http://localhost:3000")
+
+    fun connect() {
+        socket.connect()
+    }
+
+    fun onConnect() {
+        socket.on(Socket.EVENT_CONNECT) {
+            Log.d("SOCKET", "CONNECTED")
+        }
+    }
+
+    fun onDisconnect() {
+        socket.on(Socket.EVENT_DISCONNECT) {
+            Log.d("SOCKET", "DISCONNECT")
+        }
+    }
+
+}

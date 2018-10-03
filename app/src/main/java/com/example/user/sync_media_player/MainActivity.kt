d2604565aad674a677c1a5d278205bd0310e58d0
hendratay <hendratay96@gmail.com>
2018-10-03 09:19:33 +0700

    setup youtube player

--- app/src/main/java/com/example/user/sync_media_player/MainActivity.kt
+++ app/src/main/java/com/example/user/sync_media_player/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.user.sync_media_player
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : YouTubeBaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupYoutubePlayer()
+    }
+
+    private fun setupYoutubePlayer() {
+        youtube_player_view.initialize(BuildConfig.YOUTUBE_PLAYER_API_KEY, object : YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean) {
+                // Google I/O 2018
+                p1?.loadVideo("Hzv5-R9XLTc")
+                p1?.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT)
+            }
+
+            override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
+            }
+        })
     }
 }

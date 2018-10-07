229ce537e8c7463d412d50eb235bf825995f5d21
hendratay <hendratay96@gmail.com>
2018-10-07 23:46:55 +0700

    setup exo player

--- app/src/main/java/com/example/user/sync_media_player/MainActivity.kt
+++ app/src/main/java/com/example/user/sync_media_player/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.user.sync_media_player
 
+import android.net.Uri
 import android.os.Bundle
+import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 
 class MainActivity : YouTubeBaseActivity() {
 
@@ -12,10 +17,11 @@ class MainActivity : YouTubeBaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupYoutubePlayer()
+        setupExoPlayer()
     }
 
     private fun setupYoutubePlayer() {
-        youtube_player_view.initialize(BuildConfig.YOUTUBE_PLAYER_API_KEY, object : YouTubePlayer.OnInitializedListener {
+/*        youtube_player_view.initialize(BuildConfig.YOUTUBE_PLAYER_API_KEY, object : YouTubePlayer.OnInitializedListener {
             override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean) {
                 // Google I/O 2018
                 p1?.loadVideo("Hzv5-R9XLTc")
@@ -24,6 +30,18 @@ class MainActivity : YouTubeBaseActivity() {
 
             override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
             }
-        })
+        })*/
     }
+
+    private fun setupExoPlayer() {
+        val player = ExoPlayerFactory.newSimpleInstance(this)
+        exo_player_view.player = player
+        val dataSourceFactory = DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "sync-media-player"))
+        val googleIO = Uri.parse("https://www.youtube.com/watch?v=Hzv5-R9XLTc")
+        val videoSource = ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(googleIO)
+        player.prepare(videoSource)
+    }
+
 }

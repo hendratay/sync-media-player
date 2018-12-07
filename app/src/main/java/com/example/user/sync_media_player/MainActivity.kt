44a5af98f0336230a75f1ee7b360de9a95ed6931
hendratay <hendratay96@gmail.com>
2018-12-07 14:51:24 +0700

    show file chooser

--- app/src/main/java/com/example/user/sync_media_player/MainActivity.kt
+++ app/src/main/java/com/example/user/sync_media_player/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.user.sync_media_player
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -8,24 +10,38 @@ import android.view.View
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.youtube.player.YouTubeBaseActivity
-import com.google.android.youtube.player.YouTubeInitializationResult
-import com.google.android.youtube.player.YouTubePlayer
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
+import android.widget.Toast
+import android.content.Intent
+import android.util.Log
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 
 class MainActivity : YouTubeBaseActivity() {
 
     companion object {
-        val BIG_BUCK_BUNNY = Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+        private const val TAG = "MainActivity"
+        private const val FILE_REQUEST_CODE = 42
+        private val BIG_BUCK_BUNNY = Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupYoutubePlayer()
-        setupExoPlayer()
+        showFileChooser()
+//        setupYoutubePlayer()
+//        setupExoPlayer()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        if (requestCode == FILE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            resultData?.data?.let { uri ->
+                Log.d(TAG, uri.path)
+            }
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.KITKAT)
@@ -63,6 +79,18 @@ class MainActivity : YouTubeBaseActivity() {
         val videoSource = ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(BIG_BUCK_BUNNY)
         exo_player_view.player = player as SimpleExoPlayer
         player.prepare(videoSource)
+    }
+
+    private fun showFileChooser() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "*/*"
+        }
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Select a File to Upload"), FILE_REQUEST_CODE)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(this, "Please install a File Manager.", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
